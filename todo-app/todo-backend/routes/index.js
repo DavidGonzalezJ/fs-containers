e313f50f6d7f5611b2b923b3917542be5f863dc4
David <davidgonzalezj97@gmail.com>
2026-09-22 18:46:10 +0200

    Exercise 10: Used Redis to save the number of added todos

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -16,4 +16,13 @@ router.get('/', async (req, res) => {
   });
 });
 
+/* GET usage statistics. */
+router.get('/statistics', async (req, res) => {
+  const addedTodos = await redis.get('added_todos')
+
+  res.send({
+    added_todos: Number(addedTodos || 0)
+  });
+});
+
 module.exports = router;
