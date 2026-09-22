4c2b734075282abe5ca835bc61fdda9603cba767
David <davidgonzalezj97@gmail.com>
2026-09-22 15:04:45 +0200

    Exercise 7: Implemented GET & PUT for individual todos

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,15 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  if (req.body.text !== undefined) req.todo.text = req.body.text
+  if (req.body.done !== undefined) req.todo.done = req.body.done
+  await req.todo.save()
+  res.send(req.todo);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
